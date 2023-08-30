efdc0a26e1cf61d54052b2955797c710d87ef864
Daniel Terreros <110783387+DanielTKC@users.noreply.github.com>
2023-08-29 19:31:28 -0500

    Fixed override vs overload

--- EBook.java
+++ EBook.java
@@ -22,6 +22,7 @@ public class EBook extends Book {
     
   }
   
+  
   public boolean checkOut(int weeks) {
     if (weeks < 1 || weeks > 4) {
         System.out.println("Ebooks can be checked out for a period of 1-4 weeks only.");
@@ -29,7 +30,7 @@ public class EBook extends Book {
     }
 
     if (super.checkOut()) {
-        adjustDueDate(weeks * 7 - 21); // subtracting the 3 weeks set by the original checkOut
+        adjustDueDate(weeks * 7 - 21); 
         return true;
     }
     return false;
