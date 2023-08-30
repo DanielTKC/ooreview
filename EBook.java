b964db0e704fe0900b24c389ace3549ff2ac795d
Daniel Terreros <110783387+DanielTKC@users.noreply.github.com>
2023-08-29 19:06:04 -0500

    Implemented adjustDueDate in EBook.

--- EBook.java
+++ EBook.java
@@ -23,22 +23,16 @@ public class EBook extends Book {
   }
   
   public boolean checkOut(int weeks) {
-    // set a dueDate for the item to be 1-4 weeks from checkout date
-    // mark item as not available
-    
     if (weeks < 1 || weeks > 4) {
-      System.out.println("Ebooks can be checked out for a period of 1-4 weeks only.");
-      return false;
-  }
-
-    if (isAvailable()) {
-      setAvailable(false);
-      setDueDate(weeks);
-      return true;
-    } else {
-      System.out.println("The item is unavailable and cannot be checked out");
-      return false;
+        System.out.println("Ebooks can be checked out for a period of 1-4 weeks only.");
+        return false;
     }
-  }
+
+    if (super.checkOut()) {
+        adjustDueDate(weeks * 7 - 21); // subtracting the 3 weeks set by the original checkOut
+        return true;
+    }
+    return false;
+}
   
 }
